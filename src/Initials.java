c240ab0342ddc24101a344588e25d1471ee4be71
alanbaza <alanbaza@alans-mbp>
2017-12-30 01:10:49 -0600

    First Commit Uploading All Files

---
+++ src/Initials.java
@@ -0,0 +1,18 @@
+/*This program prints a header followed by my initials in block letters 
+by utilizing multiple System.out.println() statements*/
+
+public class Initials {
+
+	public static void main(String args[]){
+		
+	System.out.println("Lab 0 written by Alan Michael Baza");
+	System.out.println();	
+	System.out.println("      AAAAA  M   M  BBBBB");
+	System.out.println("      A   A  M M M  B   B");
+	System.out.println("      AAAAA  M   M  BBBB");
+	System.out.println("      A   A  M   M  B   B");
+	System.out.println("      A   A  M   M  BBBBB");
+	}
+
+}
+    
